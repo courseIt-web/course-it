0b93aeb39efd8e0bdbe0e4ad45792270af823fb6
courseIt-web <courseit4040@gmail.com>
2025-03-09 00:10:45 +0530

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -277,7 +277,7 @@ st.markdown("""
 html_file = "index.html"  # Ensure this file is in the same directory as your app
 if st.button("Looking for a Job?"):
     st.switch_page("pages/index.py")
-if st.button(MContact mentor"):
+if st.button("Contact Mentor"):
     st.switch_page("pages/payment.py")
   
   

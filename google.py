52575c0c6dc4449c6d10160ce91356bdfec9414a
courseIt-web <courseit4040@gmail.com>
2025-03-08 17:38:57 +0000

    Update google.py

--- google.py
+++ google.py
@@ -171,7 +171,7 @@ st.markdown("""
 <div style="text-align: center; margin-bottom: 40px;">
     <div class="logo-container">
         <div class="logo">
-            <img src="https://static.vecteezy.com/system/resources/previews/013/948/549/original/google-logo-on-transparent-white-background-free-vector.jpg" width="60" height="60">
+            <img src="https://clipartcraft.com/images/google-logo-transparent-2.png" width="60" height="60">
         </div>
     </div>
     <h1>Google Learning Resources</h1>

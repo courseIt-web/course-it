f7fd557fbc31e687d84e614076ccb581c4ed67b6
courseIt-web <courseit4040@gmail.com>
2025-03-08 11:26:49 +0000

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -274,27 +274,46 @@ st.markdown("""
     </div>
     """, unsafe_allow_html=True)
    
-html_file = "index.html"  # Ensure this file is in the same directory as your app
+# html_file = "index.html"  # Ensure this file is in the same directory as your app
+# if st.button("Looking for a Job?"):
+#     if os.path.exists(html_file):
+#         # Add a horizontal rule for separation
+#         st.markdown(
+#             """
+#             <style>
+#                 hr {
+#                     margin: 20px auto;
+#                     width: 80%;
+#                     border: 1px solid #ccc;
+#                 }
+#             </style>
+#             <hr>
+#             """,
+#             unsafe_allow_html=True
+#         )
+
+#         # Read and display the HTML file
+#         with open(html_file, "r", encoding="utf-8") as f:
+#             html_content = f.read()
+#         st.components.v1.html(html_content, height=600, scrolling=True)
+  
+  
+  
 if st.button("Looking for a Job?"):
-    if os.path.exists(html_file):
-        # Add a horizontal rule for separation
-        st.markdown(
-            """
-            <style>
-                hr {
-                    margin: 20px auto;
-                    width: 80%;
-                    border: 1px solid #ccc;
-                }
-            </style>
-            <hr>
-            """,
-            unsafe_allow_html=True
+    st.markdown(
+    """
+    <style>
+    hr {
+        margin: 20px auto;
+        width: 80%;
+        border: 1px solid #ccc;
+            }
+        </style>
+        <hr>
+        """,
+        unsafe_allow_html=True
         )
 
         # Read and display the HTML file
-        with open(html_file, "r", encoding="utf-8") as f:
-            html_content = f.read()
-        st.components.v1.html(html_content, height=600, scrolling=True)
-    else:
-        st.error(f"File not found: {html_file}")
+    st.switch_page("pages/google.py")
+    
